f72d730d98ab4fb7a100e29d8ee80387b78993bf
rikuyu <rikuyu5884@gmail.com>
2021-02-04 15:41:47 +0900

    ADD: column username to User, Login状況に応じた画面遷移調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
     @feeds = Photo.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
   end
